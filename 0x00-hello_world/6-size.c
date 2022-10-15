0a71d4e8b045043f901ef16f7599c44326056b59
Milcah77 <Milcahmutuku17@gmail.com>
2022-10-14 19:46:04 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,15 @@
-#include <stdio.h>                                                                                                                      
-
-/**                                                                                                                                     
- *
- *  * main - Entry                                                                                                                         
- *
- *   * Return: Always 0                                                                                                                     
- *
- *    */                                                                                                                                     
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	        printf("size of a char: %1 byte(s)\n", sizeof(char));                                                                           
-
-		        printf("size of an int: %1 byte(s)\n",sizeof(int));                                                                             
-
-			        printf("size of a long int: %1 byte(s)\n",sizeof(long int));                                                                    
-
-				        printf("size of long long int: %1 byte(s)\n",sizeof(long long int));                                                            
-
-					        printf("size of a float: %1 byte(s)\n",sizeof(float));                                                                          
-
-						        return                    
+#include <stdio.h>
+/**
+ * main - print out sizes of data types in c
+ * code by Milcah
+ * Return: 0
+ */
+int main(void)
+{
+	printf("size of a char: %1 byte(s)\n", sizeof(char));
+	printf("size of an int: %1 byte(s)\n", sizeof(int));
+	printf("size of a long int: %1 byte(s)\n", sizeof(long int));
+	printf("size of long long int: %1 byte(s)\n", sizeof(long long int));
+	printf("size of a float: %1 byte(s)\n", sizeof(float));
+	return (0);
 }
