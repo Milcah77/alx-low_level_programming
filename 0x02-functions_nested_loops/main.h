d16914d9f23eed628312ed1a2941f7d5136c42b5
Milcah77 <Milcahmutuku17@gmail.com>
2022-10-19 16:09:54 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,17 +5,13 @@ void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int);
-void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
 void print_alphabet_x10(void);
 int_isalpha(int c);
-int print_sign(int n); 
+int print_sign(int n);
 int print_last_digit(int);
-#endif
-
-
-
-	
+void jack_bauer(void);
+#endif	
