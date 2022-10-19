fad095b236529b873ac7730af8b2e24730c306bd
Milcah77 <Milcahmutuku17@gmail.com>
2022-10-19 04:37:37 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print _alphabt - prints the alpha in lowercase
+ */
+void print _alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar (la);
+	_putchar('\n');
+}
