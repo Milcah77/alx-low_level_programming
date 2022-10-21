12a173a167cbde252b50acfed3a7b8f8d300337d
Milcah77 <Milcahmutuku17@gmail.com>
2022-10-21 15:11:50 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,20 @@
 #include "main.h"
 
 /**
- * print_square - prints a square
- * @size: size of the square
+ * print_line - prints a straight line using '_'
+ * @n: the number of '_' to be printed
  */
-void print_square(int size)
+void print_line(int n)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i, j;
+	int len;
 
-		for (i = 0; i < size; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
 	}
+
+	_putchar('\n');
 
 }
 
