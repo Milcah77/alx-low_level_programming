b09769925f1aa43e8b652ab079a54ebb3adc8596
Milcah77 <Milcahmutuku17@gmail.com>
2022-10-28 19:00:51 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * cap_string - capitalizes words
  * @str: string
- * Return: pointer to string
+ * Return: pointerto string
  */
 char *cap_string(char *str)
 {
@@ -12,6 +12,7 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
+
 			i++;
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
@@ -27,11 +28,9 @@ char *cap_string(char *str)
 		    str[i - 1] == '{' ||
 		    str[i - 1] == '}' ||
 		    i == 0)
-			str[i] - = 32;
+			str[i] -= 32;
 		i++;
 	}
 
 	return (str);
 }
-
-
